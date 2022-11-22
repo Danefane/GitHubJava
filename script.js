95059da005d579df58a59db772447b38adbef861
Danefane <lian.t.fjeld@gmail.com>
2022-11-22 11:10:10 +0100

    Edited javascript
    Removed experiment codes and edited the main codes
    
    #3

--- script.js
+++ script.js
@@ -1,50 +1,17 @@
-// {/* <!-- This is the textbox--> */}
-// function addClick(){
-// var option = document.createElement("li");
-// var input = document.getElementById("addInput").value;
-// var textnode = document.createElement("p");
-// textnode.innerText = input;
-// document.getElementById("ulList").appendChild(option);
-// option.appendChild(textnode);
-// }
+{/* <!-- This is the textbox--> */}
+function addClick(){
+    console.log("addClick");
+    var option = document.createElement("li");
+    var input = document.getElementById("addInput").value;
+    var textnode = document.createElement("p");
+    textnode.innerText = input;
+    document.getElementById("ulList").appendChild(option);
+    option.appendChild(textnode);
+}
 
-
-let input = document.querySelector('.enterList');
-let addBtn = document.querySelector('.addList');
-let tasks = document.querySelector('.task');
-
-// add btn disabled
-
-input.addEventListener('keyup', () => {
-    if(input.value.trim() !== 0){
-    } else {
-        addBtn.classList.remove('active')
-    }
-})
-
-// add new item to list
-
-addBtn.addEventListener('click', () => {
-  if (InputDeviceInfo.value.trim() !== 0) {
-    let newItem = document.createElement('div');
-    newItem.classList.add('item');
-    newItem.innerHTML = `
-    <p> $(input.value) </p>
-    <div class="item-btn">
-      <i class="fa-solid fa-pen-to-square"></i>
-      <i class="fa-solid fa-xmark"></i>
-    </div>
-    `
-    tasks.appendChild(newItem);
-    input.value = '';
-  } else {
-      alert('Please enter a task')
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
   }
-} )
-
-// delete item from list
-
-tasks.addEventListener('click', (e) => {
-  if (e.target.classList.contains('fa-xmark'))
-      e.target.parentElement.parentElement.remove();
-})
+}, false);
